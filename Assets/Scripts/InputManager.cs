242d709c8dacf365f2f658190a2c53089d851ddb
Kuldeep Singh <kuldeep_reck@yahoo.com>
2020-04-11 18:07:29 +0530

    hello XR first commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject cubePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+      Touch touch;
+
+      if (Input.touchCount > 0 && (touch = Input.GetTouch(0)).phase == TouchPhase.Began)
+      {
+        CreateCube(touch.position);
+      }
+      else if (Input.GetMouseButtonDown(0))
+      {
+        CreateCube(Input.mousePosition);
+      }
+    }
+
+    void CreateCube(Vector2 position)
+    {
+      Ray ray = camera.ScreenPointToRay(position);
+      Instantiate(cubePrefab, (ray.origin + ray.direction), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private Vector3 rotation = new Vector3(20, 30, 0);
+    void Update()
+    {
+      transform.Rotate(rotation * Time.deltaTime);
+    }
+}
